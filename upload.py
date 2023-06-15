7aae352cb3e4c7c0244c4b9478cb56c568a3071c
Tanel Treuberg <tanel.treuberg@elering.ee>
2023-06-15 15:17:14 +0300

    Confluence uploader with ini file configured

--- upload.py
+++ upload.py
@@ -1,17 +1,20 @@
+from configparser import ConfigParser
 from atlassian import Confluence
 from pathlib import Path
 
-file = "file.csv"
-path = Path("") / file
+conf = ConfigParser()
+conf.read("config.ini")
+
+path = Path(conf["File_params"]["absolute_path"])
 
 # Log in to Confluence
 confluence = Confluence(
-    url="",
-    username="",
-    password="",
+    url=conf["Params"]["url"],
+    username=conf["Params"]["username"],
+    password=conf["Params"]["password"],
     verify_ssl = False
 )
 
 # Attach (upload) a file to a page, if it exists it will update the
 # automatically version the new file and keep the old one
-confluence.attach_file(path, name=None, content_type="text/csv", page_id="316211592", title=None, space=None, comment="")
+confluence.attach_file(path, name=None, content_type=conf["File_params"]["content_type"], page_id=conf["Params"]["page_id"], title=None, space=None, comment="")
